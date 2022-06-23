71924763e8d643f3db4c4eb96089157c9554054d
Aidan Antony <aidanantony65@gmail.com>
2022-06-23 12:51:36 -0600

    Clean up of seeding

--- db/seeds/04_line_items.sql
+++ db/seeds/04_line_items.sql
@@ -1,1 +1,3 @@
 INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (1, 2, 2);
+INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (2, 4, 2);
+INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (3, 7, 2);
