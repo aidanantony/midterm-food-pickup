7d17a8942266c33c8deaf8a56463f737feb294f6
Aidan Antony <aidanantony65@gmail.com>
2022-06-23 08:50:45 -0600

    Modified create tables for user orders and line items

--- db/schema/03_user_orders.sql
+++ db/schema/03_user_orders.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS user_orders CASCADE;
 CREATE TABLE user_orders (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id)
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   prep_time VARCHAR(255),
   date TIMESTAMP,
 );
--- db/schema/04_line_items.sql
+++ db/schema/04_line_items.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS line_items CASCADE;
 CREATE TABLE line_items (
   id SERIAL PRIMARY KEY NOT NULL,
-  food_item_id INTEGER REFERENCES food_items(id)
-  user_order_id INTEGER REFERENCES user_orders(id)
+  food_item_id INTEGER REFERENCES food_items(id) ON DELETE CASCADE,
+  user_order_id INTEGER REFERENCES user_orders(id) ON DELETE CASCADE
 );
