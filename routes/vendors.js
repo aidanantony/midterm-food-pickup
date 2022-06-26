241806ea5fbc254f170489657d5b1fb024593679
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-06-25 22:19:33 -0600

    Displaying hard coded information on views

--- routes/vendors.js
+++ routes/vendors.js
@@ -9,13 +9,25 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM vendors`;
+    let query = `SELECT user_orders.id as orderNumber, user_orders.prep_time, user_orders.current_status, food_items.name as itemName, count(food_items.name) as quantity,
+    users.name as name, users.phone_number as phone FROM user_orders
+    JOIN line_items ON line_items.user_order_id = user_orders.id
+    JOIN food_items ON line_items.food_item_id = food_items.id
+    JOIN users ON users.id = user_orders.user_id
+    GROUP BY orderNumber, itemName, users.name, users.phone_number`;
+    // let query = `SELECT food_items.name as itemName, count(food_items.name) as quantity FROM food_items
+    // JOIN line_items ON line_items.food_item_id = food_items.id
+    // JOIN user_orders ON line_items.user_order_id = user_orders.id
+    // GROUP BY itemName`
     console.log(query);
     db.query(query)
       .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
+        const orders = data.rows;
+        //const filteredData = filterData(orders);
+        const filtered = updateOrders(orders);
+        res.json({ filtered });
       })
       .catch(err => {
         res
@@ -23,5 +35,11 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  // router.get("/", (req, res) => {
+  //   res.render("vendorInterface");
+  // });
+
+
   return router;
 };
