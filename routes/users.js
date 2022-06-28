6cdc15a48208fb8e247be573cd46a71aff4513f7
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-06-28 10:54:53 -0600

    add user order id first inteh DB before sdding line items to make user order number available before posting onto line-items

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,8 @@ const router  = express.Router();
 const { sendMessageToClient } = require('../public/scripts/send_sms_twilio');
 
 
-module.exports = (db) => { //render food menu from DB
-  router.get("/", (req, res) => {
+module.exports = (db) => {
+  router.get("/", (req, res) => {//render food menu from DB
     db.query(`SELECT * FROM food_items;`)
       .then(data => {
         const foodItemsForMenu = data.rows;
@@ -25,25 +25,48 @@ module.exports = (db) => { //render food menu from DB
       });
   });
 
+  // router.post("/orders", (req, res) => {
+  //   const userOrder = req.body.food; //bring food items id from front to back
+  //   for (let foodItemId of userOrder) {
+  //     const foodItemNumberFromUser = parseInt(foodItemId);
+  //     db.query(`SELECT * FROM food_items WHERE id = ${foodItemNumberFromUser};`)
+  //       .then(data => { //getting corresponding data from database
+  //         const foodItemDetailsFromDatabase = data.rows;
+  //         const insertOrderDetailsInLineItemsTable = `INSERT INTO line_items(food_item_id,user_order_id) VALUES ($1, $2);`;
+  //         db.query(insertOrderDetailsInLineItemsTable, [foodItemDetailsFromDatabase[0].id, 2]); //adding line_items for for users.id = 2
+  //       });
+  //   }
+  //   const insertUserInfoInUserOrdersTable = `INSERT INTO user_orders(user_id, prep_time, current_status) VALUES ($1, $2, $3) RETURNING id;`; //for user.id = 2, hardcoding user atm, //getting newly inserted data id right away
+  //   db.query(insertUserInfoInUserOrdersTable, [2, 0, 'In progress']).then((orderId) => {
+  //     //console.log(orderId.rows[0].id);//needs to be removed later - fo rdebugging purposes only //bring in newly inserted order #
+  //     const newOrderId = orderId.rows[0].id; //extraching order ID from db returned results
+  //     db.query(`SELECT users.phone_number FROM users JOIN user_orders ON users.id = user_orders.user_id WHERE user_orders.id = $1;`, [ newOrderId ]).then((phoneNumber) => {
+  //       const receiverPhoneNumber = phoneNumber.rows[0].phone_number;
+  //       //console.log('receiverPhoneNumber:', receiverPhoneNumber);
+  //       const messageForReceiver = `Thank you for ordering with FoodTruck! You order number is ${newOrderId}.`; //creating msg string
+  //       //receiverPhoneNumber = '+17802154894'; //need to erase eventually
+  //       sendMessageToClient(messageForReceiver, receiverPhoneNumber);
+  //       res.render('customerOrderConfirmation', { newOrderId });
+  //     });
+  //   }); //adding personal phone number for now, should be able to fetch users number from db
+
+  // });
+
   router.post("/orders", (req, res) => {
-    console.log(req.body.food[0]);
-    const userOrder = req.body.food;
-    for (let foodItemId of userOrder) {
-      const foodItemNumberFromUser = parseInt(foodItemId);
-      db.query(`SELECT * FROM food_items WHERE id = ${foodItemNumberFromUser};`)
-        .then(data => {
-          const foodItemDetailsFromDatabase = data.rows;
-          // console.log(foodItemDetailsFromDatabase[0].id);
-          const insertOrderDetailsInLineItemsTable = `INSERT INTO line_items(food_item_id,user_order_id) VALUES ($1, $2);`;
-          db.query(insertOrderDetailsInLineItemsTable, [foodItemDetailsFromDatabase[0].id, 2]);
-          console.log('User Order: ', foodItemDetailsFromDatabase);
-        });
-    }
-    const insertUserInfoInUserOrdersTable = `INSERT INTO user_orders(user_id, prep_time, current_status) VALUES ($1, $2, $3) RETURNING id;`; //for user.id = 2, hardcoding user atm, //getting newly inserted data id right away
-    db.query(insertUserInfoInUserOrdersTable, [2, 0, 'In progress']).then((orderId) => {
-      //console.log(orderId.rows[0].id);//needs to be removed later - fo rdebugging purposes only //bring in newly inserted order #
+    //inserrting order info in user_orders to extract user_orders.id right away
+    const insertUserInfoInUserOrdersTable = `INSERT INTO user_orders(user_id, prep_time, current_status) VALUES ($1, $2, $3) RETURNING id;`;
+    db.query(insertUserInfoInUserOrdersTable, [2, 0, 'In progress']).then((orderId) => { //2 will be replaced by logged in user info
       const newOrderId = orderId.rows[0].id; //extraching order ID from db returned results
-      db.query(`SELECT users.phone_number FROM users JOIN user_orders ON users.id = user_orders.user_id WHERE user_orders.id = $1;`, [ newOrderId ]).then((phoneNumber) => {
+      const userOrder = req.body.food; //bringing food items id from front to back
+      for (let foodItemId of userOrder) {
+        const foodItemNumberFromUser = parseInt(foodItemId);
+        db.query(`SELECT * FROM food_items WHERE id = ${foodItemNumberFromUser};`)
+          .then(data => { //getting corresponding data from database for line_items insert
+            const foodItemDetailsFromDatabase = data.rows;
+            const insertOrderDetailsInLineItemsTable = `INSERT INTO line_items(food_item_id,user_order_id) VALUES ($1, $2);`;
+            db.query(insertOrderDetailsInLineItemsTable, [foodItemDetailsFromDatabase[0].id, newOrderId]); //adding line_items for for users.id = 2
+          });
+      } db.query(`SELECT users.phone_number FROM users JOIN user_orders ON users.id = user_orders.user_id WHERE user_orders.id = $1;`, [     newOrderId ]).then((phoneNumber) => {
         const receiverPhoneNumber = phoneNumber.rows[0].phone_number;
         //console.log('receiverPhoneNumber:', receiverPhoneNumber);
         const messageForReceiver = `Thank you for ordering with FoodTruck! You order number is ${newOrderId}.`; //creating msg string
@@ -54,6 +77,7 @@ module.exports = (db) => { //render food menu from DB
     }); //adding personal phone number for now, should be able to fetch users number from db
 
   });
+
 
   return router;
 };
