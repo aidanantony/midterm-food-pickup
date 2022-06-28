385a59568463e932f396495ead70bb55c5608418
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-06-28 01:30:54 -0600

    Add new order

--- db/seeds/03_user_orders.sql
+++ db/seeds/03_user_orders.sql
@@ -1,5 +1,6 @@
 INSERT INTO user_orders (id, user_id, prep_time, date, current_status ) VALUES (1, 1, '20', Now(), 'Ready for pickup');
 INSERT INTO user_orders (id, user_id, prep_time, date, current_status ) VALUES (2, 2, '30', Now(), 'Complete');
 INSERT INTO user_orders (id, user_id, prep_time, date, current_status ) VALUES (3, 1, '25', Now(), 'In progress');
+INSERT INTO user_orders (id, user_id, prep_time, date, current_status ) VALUES (4, 2, 'N/A', Now(), 'New Order');
 
 
--- db/seeds/04_line_items.sql
+++ db/seeds/04_line_items.sql
@@ -1,3 +1,8 @@
 INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (1, 2, 2);
 INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (2, 4, 2);
 INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (3, 7, 2);
+INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (4, 7, 4);
+INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (5, 8, 4);
+INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (6, 1, 4);
+INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (7, 6, 4);
+INSERT INTO line_items (id, food_item_id, user_order_id ) VALUES (8, 6, 4);
