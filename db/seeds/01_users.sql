311085d0240fbbb56c21e94d0a636e4df15f234b
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-06-28 10:27:13 -0600

    grab phone # from database and sedn order # to the customer

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (id, name, email, phone_number, admin) VALUES (1, 'Alice Allison', 'alice@gmail.com', '403-987-1234', false);
-INSERT INTO users (id, name, email, phone_number, admin) VALUES (2, 'Kira Baker', 'kira@gmail.com', '403-123-4567', false);
-INSERT INTO users (id, name, email, phone_number, admin) VALUES (3, 'Chase Bradley', 'chase@gmail.com', '403-123-6789', true);
+INSERT INTO users (name, email, phone_number, admin) VALUES ('Alice Allison', 'alice@gmail.com', '403-987-1234', false);
+INSERT INTO users (name, email, phone_number, admin) VALUES ('Kira Baker', 'kira@gmail.com', '+17802154894', false);
+INSERT INTO users (name, email, phone_number, admin) VALUES ('Chase Bradley', 'chase@gmail.com', '403-123-6789', true);
--- routes/users.js
+++ routes/users.js
@@ -7,16 +7,16 @@
 
 const express = require('express');
 const router  = express.Router();
-const { sendMessageToClient } = require('../public/scripts/send_sms_customer');
+const { sendMessageToClient } = require('../public/scripts/send_sms_twilio');
 
 
-module.exports = (db) => {
+module.exports = (db) => { //render food menu from DB
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM food_items;`)
       .then(data => {
         const foodItemsForMenu = data.rows;
         console.log(foodItemsForMenu);
-        res.render('customerInterface', {foodItemsForMenu});
+        res.render('customerInterface', { foodItemsForMenu });
       })
       .catch(err => {
         res
@@ -40,14 +40,19 @@ module.exports = (db) => {
         });
     }
     const insertUserInfoInUserOrdersTable = `INSERT INTO user_orders(user_id, prep_time, current_status) VALUES ($1, $2, $3) RETURNING id;`; //for user.id = 2, hardcoding user atm, //getting newly inserted data id right away
-    db.query(insertUserInfoInUserOrdersTable, [2, 0, 'In progress']).then((data) => {
-      console.log(data.rows[0].id);
-      const newOrderId = data.rows[0].id; //extraching order ID from db returned results
-      const messageForReceiver = `You order number is ${newOrderId}.`; //creating msg string
-      const receiverPhoneNumber = '+17802154894';
-      sendMessageToClient(messageForReceiver, receiverPhoneNumber);
-    }); //adding personal ohone number for now, should be able to fetch users number from db
-    res.render('customerOrderConfirmation');
+    db.query(insertUserInfoInUserOrdersTable, [2, 0, 'In progress']).then((orderId) => {
+      //console.log(orderId.rows[0].id);//needs to be removed later - fo rdebugging purposes only //bring in newly inserted order #
+      const newOrderId = orderId.rows[0].id; //extraching order ID from db returned results
+      db.query(`SELECT users.phone_number FROM users JOIN user_orders ON users.id = user_orders.user_id WHERE user_orders.id = $1;`, [ newOrderId ]).then((phoneNumber) => {
+        const receiverPhoneNumber = phoneNumber.rows[0].phone_number;
+        //console.log('receiverPhoneNumber:', receiverPhoneNumber);
+        const messageForReceiver = `Thank you for ordering with FoodTruck! You order number is ${newOrderId}.`; //creating msg string
+        //receiverPhoneNumber = '+17802154894'; //need to erase eventually
+        sendMessageToClient(messageForReceiver, receiverPhoneNumber);
+        res.render('customerOrderConfirmation', { newOrderId });
+      });
+    }); //adding personal phone number for now, should be able to fetch users number from db
+
   });
 
   return router;
